c1d8a7a7efa6c0f0e6fa53276f3a9c1761d3a734
Ahmad Naufal <naufalopal002@gmail.com>
2025-07-29 12:37:11 +0700

    refactor: :recycle: refactor  uuid albums

--- src/services/postgres/AlbumsService.js
+++ src/services/postgres/AlbumsService.js
@@ -11,7 +11,7 @@ class AlbumsService {
 
   // Create album service
   async addAlbum({ name, year }) {
-    const id = nanoid(16);
+    const id = `album-${nanoid(16)}`;
     const createdAt = new Date().toISOString();
     const updateAt = createdAt;
 
