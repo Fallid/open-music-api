eaae31c26599157944f05f75bcfa75dc0e27cfa5
Ahmad Naufal <naufalopal002@gmail.com>
2025-09-11 17:00:07 +0700

    perf: improve business flow
    song cannot duplicate or exist the same song more than 1 in playlist

--- src/services/postgres/PlaylistSongsService.js
+++ src/services/postgres/PlaylistSongsService.js
@@ -7,7 +7,19 @@ class PlaylistSongsService {
     this._pool = new Pool();
   }
 
+  async verifyNewSongInPlaylist(songId) {
+    const query = {
+      text: 'SELECT song_id FROM playlist_songs WHERE song_id = $1',
+      values: [songId],
+    };
+    const result = await this._pool.query(query);
+    if (result.rowCount > 0) {
+      throw new InvariantError('Gagal menambahkan song. Song sudah ada di playlist.');
+    }
+  }
+
   async addPlaylistSong(playlistId, songId) {
+    await this.verifyNewSongInPlaylist(songId);
     const id = `playlist_song-${nanoid(16)}`;
     const query = {
       text: 'INSERT INTO playlist_songs VALUES($1, $2, $3) RETURNING id',
