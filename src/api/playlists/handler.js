c5f497c6d46d4f04534b2647eba2d39361ee1953
Ahmad Naufal <naufalopal002@gmail.com>
2025-09-11 09:36:43 +0700

    feat: playlist plugin for create new playlist

---
+++ src/api/playlists/handler.js
@@ -0,0 +1,29 @@
+const autoBind = require('auto-bind');
+
+class PlaylistsHandler {
+  constructor(service, validator) {
+    this._service = service;
+    this._validator = validator;
+
+    autoBind(this);
+  }
+
+  async postPlaylistHandler(request, h) {
+    this._service.validatePostPlaylistPayload(request.payload);
+    const { name } = request.payload;
+    const { id: credentialId } = request.auth.credentials;
+
+    const playlistId = await this._service.addPlaylist({ name, owner: credentialId });
+
+    const response = h.response({
+      status: 'success',
+      data: {
+        playlistId,
+      },
+    });
+    response.code(201);
+    return response;
+  }
+}
+
+module.exports = PlaylistsHandler;
