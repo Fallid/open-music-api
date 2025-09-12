6e553a273e7c47075f7a5fbf69606daba867a531
Ahmad Naufal <naufalopal002@gmail.com>
2025-09-12 16:25:06 +0700

    feat: collaborations service
    owner can add collaborator into their playlist

---
+++ src/validators/collaborations/index.js
@@ -0,0 +1,14 @@
+const InvariantError = require('../../exceptions/InvariantError');
+const { collaborationsPayloadSchema } = require('./schema');
+
+const CollaborationsValidator = {
+  validateCollaborationPayload: (payload) => {
+    const validationResult = collaborationsPayloadSchema.validate(payload);
+
+    if (validationResult.error) {
+      throw new InvariantError(validationResult.error.message);
+    }
+  },
+};
+
+module.exports = CollaborationsValidator;
